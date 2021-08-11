768ac7c706b16d4803db9832c0ac26c76566ed61
albertosantes21@hotmail.com <alberto.23>
2021-08-10 23:48:31 -0500

    modifico el redireccionamiento del index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 //require "configs/config.php";
+$install = 'install.php?shop=';
 if (isset($_GET['shop'])) {
     $shop = explode(".", $_GET['shop']);
     if (!empty($shop[0])) {
-        header("Location: install.php?shop=" . $shop[0]);
-    //    die();
+        header('Location: '.$install. $shop[0]);
+        die();
     } else {
         echo "Verifica tu código";
     }
