c049e040b2a488f33348085e0094f2644b0af79e
albertosantes21@hotmail.com <alberto.23>
2021-08-10 22:59:07 -0500

    agrego scopes al proyecto

--- configs/config.php
+++ configs/config.php
@@ -3,9 +3,17 @@
  * Configuracion para shopify
  */
 
+$scopes = "read_orders";
+$scopes += "write_products,";
+$scopes += "read_product_listings,";
+$scopes += "read_themes,write_themes,";
+$scopes += "read_script_tags,write_script_tags,";
+$scopes += "read_checkouts,write_checkouts";
+
 $shopify = array(
         'API_SECRET' => 'f3ab52969d5d358540b9db83123d8ba9',
         'SHARED_SECRET' => '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',
+        'SCOPRES'   => $scopes,
 );
 
 /*
--- install.php
+++ install.php
@@ -4,7 +4,7 @@ require "configs/config.php";
 // Set variables for our request
 $shop = $_GET['shop'];
 $api_key = $shopify['API_KEY'];
-$scopes = "read_orders,write_products";
+$scopes = $shopify['SCOPRES'];
 $redirect_uri = $app_config['TOKEN'];
 
 // Build install/approval URL to redirect to
